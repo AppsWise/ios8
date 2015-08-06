7980fc373cad2c3dee8987cc624e673ad0372ae5
AppsWise <support@appswise.io>
2015-08-06 22:06:55 +0530

    UI Picker View Included
    Added UI Picker View

--- DatePickerInStaticCell/DatePickerInStaticCell/TableViewController.swift
+++ DatePickerInStaticCell/DatePickerInStaticCell/TableViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController,UIPickerViewDelegate,UIPickerViewDataSource {
     
     @IBOutlet weak var toggle: UISwitch!
     @IBOutlet weak var startDate: UILabel!
     @IBOutlet weak var endDate: UILabel!
     var startDatePickerVisible = false
     var endDatePickerVisible = false
+    var pickerViewVisible = false
+    @IBOutlet weak var typeLabel: UILabel!
+    var types = ["Business Trip","Personal Tour","Holiday"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -54,6 +57,10 @@ class TableViewController: UITableViewController {
             endDatePickerVisible = !endDatePickerVisible
             tableView.reloadData()
         }
+        if indexPath.row == 4{
+            pickerViewVisible = !pickerViewVisible
+            tableView.reloadData()
+        }
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
@@ -68,82 +75,31 @@ class TableViewController: UITableViewController {
                 return 0.0
             }
             return 165.0
-        
+            
+        }else if indexPath.row == 5 {
+            if !pickerViewVisible  {
+                return 0.0
+            }
+            return 165.0
+            
         }
         return 44.0
     }
     
-//    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-//        return 44.0
-//    }
-    
-    // MARK: - Table view data source
-    /*
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    // #warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0
+    // MARK UIPickerView Delegate
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return types[row]
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        typeLabel.text = types[row]
     }
-    */
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
     
-    // Configure the cell...
-    
-    return cell
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return types.count
     }
-    */
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return NO if you do not want the specified item to be editable.
-    return true
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+        return 1
     }
-    */
-    
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-    
-    }
-    */
-    
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return NO if you do not want the item to be re-orderable.
-    return true
-    }
-    */
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
 }
